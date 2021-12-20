e1d31e490f7e9a32e285499e56b0dc56a60816ac
uthsobcb <uthsob9@gmail.com>
2021-12-21 02:47:14 +0600

    Initial all file added

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello Peter</h1>
+    <p>
+      Hello I'm learning Vue.js. This is a project bulit while learning vue js
+      from <a href="https://www.vuemastery.com/">Vuemastery</a>
+    </p>
+  </div>
+</template>
+
+<script></script>
+<style></style>
